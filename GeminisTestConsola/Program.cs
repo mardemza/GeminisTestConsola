7a04cc25ce926b235291e147a6a270b5cec43cec
Moises Rivas <moises.rivas@metrodev.com.ar>
2022-06-06 14:14:54 -0300

    Ejercicio 2 - TERMINADO

--- GeminisTestConsola/Program.cs
+++ GeminisTestConsola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace GeminisTestConsola
@@ -8,24 +9,104 @@ namespace GeminisTestConsola
 
         static void Main(string[] args)
         {
-            #region Ejercio 1            
+            #region Ejercicio 1  
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("");
             Console.WriteLine("Ej.1:");
 
-            var n = 15;
+            var n = 5;
             Console.WriteLine($"Input:{n}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Result:");
+            Console.WriteLine("");
 
             for (int i = 0; i < n; i++)
             {
+                // -- Reviso si par o impar para determinar con que letra debe comenzar
                 var beginX = (i % 2) == 0;
                 var line = new StringBuilder();
 
                 for (int j = 0; j < n; j++)
                 {
+                    // -- Detecto que caracte char deberia ingresar
                     var chartToAdd = beginX && j == 0 ? "X" : line.ToString().EndsWith("_") ? "X" : "_";
+
+                    // -- Agrego caracter 
                     line.Append(chartToAdd);
+                }
+
+                // -- Imprimo linea
+                Console.WriteLine(line.ToString());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("");
+            #endregion
+
+            #region Ejercicio 2
+            Console.WriteLine("Ej.2:");
+
+            int[] myArray = new int[] { 1, 2, -1, 1, 0, 1, 2, -1, -1, -2 };
+            Console.WriteLine($"Input: [{string.Join(", ", myArray)}]");
+
+            Console.WriteLine("");
+
+            var c = 4;
+            var derecha = 0;
+            var abajo = 0;
+
+            for (int i = 0; i <= myArray.Length - 1; i += 2)
+            {
+                // -- Evaluar derecha
+                var irDerecha = myArray[i] + derecha;
+                if (irDerecha <= c - 1 && irDerecha >= 0)
+                {
+                    derecha = irDerecha;
+                }
+                else if (irDerecha < 0)
+                {
+                    derecha = 0;
+                }
+                else if (irDerecha > c - 1)
+                {
+                    derecha = c - 1;
+                }
+
+                // -- Evaluar abajo
+                var irAbajo = myArray[i + 1] + abajo;
+                if (irAbajo <= c - 1 && irAbajo >= 0)
+                {
+                    abajo = irAbajo;
+                }
+                else if (irAbajo < 0)
+                {
+                    abajo = 0;
+                }
+                else if (irAbajo > c - 1)
+                {
+                    abajo = c - 1;
+                }
+            }
+
+            Console.WriteLine("Result:");
+            Console.WriteLine("");
+
+            for (int a = 0; a < c; a++)
+            {
+                var line = new StringBuilder();
+                for (int d = 0; d < c; d++)
+                {
+                    var chartToAdd = (d == derecha && a == abajo) ? "X" : "0";
+                    line.Append(chartToAdd);
+
                 }
                 Console.WriteLine(line.ToString());
             }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("");
             #endregion
         }
     }
--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 - Proyecto para 3 ejercicios
 - Debe tener instalado NET 5
 
-## Ejercicio 1
-Descripción:
+### Ejercicio 1
+*Descripción:* \
 Imprimir un tablero de damas donde la "X" representa el color negro y el "_" representa el blanco. 
 El tablero debe tener n x n casillas. Por ejemplo, para n=5 el tablero se vería así:
 
@@ -28,3 +28,24 @@ El código para el tamaño de n ya está ahí, puede editarlo para probar con ot
 hacer clic en el botón de actualización junto a él para volver al valor original que se utiliza 
 para validar su código durante la prueba. Tenga en cuenta que el código debe imprimir los resultados 
 exactamente como se muestra con el fin de que la pregunta sea considerada valida durante la prueba.
+
+### Ejercicio 2
+*Descripción:* \
+Se tiene una X en la esquina superior izquierda de un área de 4x4. Se tiene una matriz con 10 elementos. 
+Cada 2 elementos de la matriz corresponden a un movimiento, el primero en el eje horizontal y el segundo 
+en el eje vertical. El número indica las unidades a moverse y el signo la dirección (positivo para derecha 
+o abajo, negativo para izquierda o arriba)
+
+Por ejemplo, para la matriz myArray:=(1,2,-1,1,0,1,2,-1,-1,-2)
+
+La X se moverá una unidad a la derecha y dos hacia abajo, luego una unidad a la izquierda y una abajo y 
+así sucesivamente. El programa a escribir debe imprimir la posición final de la X. Para representar los 
+lugares donde la X no se encuentra utilizar la letra O. Si la instrucción obliga a la X a salir del área 
+de 4x4 la X permanecerá en el borde, sin salir. Para el arreglo presentado el resultado se vería así:
+
+```
+OXOO
+OOOO
+OOOO
+OOOO
+```
