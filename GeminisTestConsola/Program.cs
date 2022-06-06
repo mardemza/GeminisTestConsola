020e7ffae22d10be55a47d59623c21ab4190bcd3
Moises Rivas <moises.rivas@metrodev.com.ar>
2022-06-06 10:00:23 -0300

    Agregar archivos de proyecto.

---
+++ GeminisTestConsola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace GeminisTestConsola
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word!");
+        }
+    }
+}
